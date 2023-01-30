588a415277f18b3b2febf8842b52d5f7dc61c139
t_kobayashi <t_kobayashi@MacBook-Pro-3.local>
2023-01-30 18:36:52 +0900

    add lottie component

---
+++ src/components/Lottie.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+const Lottie = () => {
+  return (
+    <div className="md:flex justify-between items-center mb-60 md:mb-78"></div>
+  );
+};
+
+export default Lottie;
