e70ad0f7e48008741ad489b00943f6d0a6651760
t_kobayashi <t_kobayashi@MacBook-Pro-3.local>
2023-01-30 18:40:46 +0900

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
+const fontSize = {};
+const spacing = {};
+
+[...Array(200).keys()].forEach((v) => {
+  fontSize[`${v}px`] = `${v}px`;
+});
+
+[...Array(2000).keys()].forEach((v) => {
+  spacing[`${v}px`] = `${v}px`;
+});
+
+const serialNumToRem = (length = 100) => {
+  const rems = {};
+  [...Array(length).keys()].forEach((v) => {
+    rems[v] = `${v / 16}rem`;
+  });
+  return rems;
+};
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
